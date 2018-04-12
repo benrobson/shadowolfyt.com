8e7be0337d3c66c2678362242482f815bd1bb016
shadowolf <shadowolfbusiness@gmail.com>
2018-04-12 16:31:13 +1000

    Compiling site resources.

---
+++ js/404GIF.js
@@ -0,0 +1,14 @@
+gif = [ 
+  	["https://media.giphy.com/media/VwoJkTfZAUBSU/giphy.gif",],
+  	["https://media.giphy.com/media/xTiN0L7EW5trfOvEk0/giphy.gif",],
+  	["https://media.giphy.com/media/l1J9EdzfOSgfyueLm/giphy.gif",],
+  	["https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif",],
+  	["https://media.giphy.com/media/Lny6Rw04nsOOc/giphy.gif",],
+  	["https://media.giphy.com/media/NTXqH1bUCfHBS/giphy.gif",],
+  	["https://media.giphy.com/media/mq5y2jHRCAqMo/giphy.gif",],
+  	["https://media.giphy.com/media/5f2mqsGTHpe5a/giphy.gif",]];
+	
+	
+window.onload = function(){
+x = Math.floor(Math.random()*gif.length); 
+	document.getElementById("404gifimg").src = gif[x][0]};
---
+++ js/alerts.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+function unavaliable() {
+    alert('This page is currently under construction and is unavaliable, please come again later!');
+}
